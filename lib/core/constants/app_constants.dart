d34d0c6044ce0c1f0fb347ceb2370c58e90bc4ea
Rama Hany <hanyrama703@gmai.com>
2026-05-07 02:12:05 +0300

    refactor app_constants.dart to separate AppImages and CacheKeys classes

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,3 +1,9 @@
-class AppConstants {
+class AppImages {
   static const String universityLogo = "assets/images/Hurghada-University.png";
+}
+
+class CacheKeys {
+  static const String userToken = "userToken";
+  static const String isDarkMode = "isDarkMode";
+  static const String language = "language";
 }
