e38c42b0d496a3b8a09206932810433aff8571d0
Rama Hany <hanyrama703@gmai.com>
2026-05-07 02:18:18 +0300

    feat: implement AuthWebServices for user authentication handling

---
+++ lib/features/auth/data/web_services/auth_web_services.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:leave_management_system/core/networking/api_endpoints.dart';
+import 'package:leave_management_system/core/networking/api_service.dart';
+import 'package:leave_management_system/features/auth/data/models/login_body_model.dart';
+import 'package:leave_management_system/features/auth/data/models/login_response_model.dart';
+
+class AuthWebServices {
+  final ApiService _apiService;
+
+  AuthWebServices({required ApiService apiService}) : _apiService = apiService;
+
+  Future<LoginResponseModel> login({required LoginBodyModel loginBody}) async {
+    Response response = await _apiService.postRequest(
+      apiEndpoint: ApiEndpoints.login,
+      data: loginBody.toJson(),
+    );
+    return LoginResponseModel.fromJson(response.data);
+  }
+}
