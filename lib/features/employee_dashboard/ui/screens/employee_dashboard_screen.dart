024c985feecbc5fe3be617fbabc3aa12d8966d7b
Rama Hany <hanyrama703@gmai.com>
2026-05-08 01:32:04 +0300

    feat: add OnboardingScreen and EmployeeDashboardScreen for user navigation

---
+++ lib/features/employee_dashboard/ui/screens/employee_dashboard_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EmployeeDashboardScreen extends StatelessWidget {
+  const EmployeeDashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("employee dashboard screen"));
+  }
+}
