e9eb7a76e5a47453cee3a0dd116bd857e32d830a
Rama Hany <hanyrama703@gmai.com>
2026-05-08 01:30:50 +0300

    feat: refactor login screen with Bloc for state management

--- lib/features/auth/ui/screens/login_screen.dart
+++ lib/features/auth/ui/screens/login_screen.dart
@@ -1,6 +1,13 @@
+import 'package:animated_snack_bar/animated_snack_bar.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:leave_management_system/core/routes/app_routes.dart';
+import 'package:leave_management_system/core/utils/animated_snack_dialogue.dart';
+import 'package:leave_management_system/features/auth/data/models/login_body_model.dart';
+import 'package:leave_management_system/features/auth/logic/cubit/auth_cubit.dart';
 import '../../../../core/constants/app_constants.dart';
 import '../../../../core/language/locale_keys.g.dart';
 import '../../../../core/styles/app_text_styles.dart';
@@ -36,72 +43,102 @@ class _LoginScreenState extends State<LoginScreen> {
             padding: EdgeInsets.symmetric(horizontal: 24.w),
             child: SizedBox(
               width: double.infinity,
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 48.h),
-                    Image.asset(
-                      AppConstants.universityLogo,
-                      height: 120.sp,
-                      width: 120.sp,
-                    ),
-                    SizedBox(height: 35.h),
-                    Text(
-                      LocaleKeys.login_title.tr(),
-                      style: AppTextStyles.black24w600TextStyle,
-                    ),
-                    SizedBox(height: 10.h),
-                    Text(
-                      LocaleKeys.login_subtitle.tr(),
-                      style: AppTextStyles.grey18w400TextStyle,
-                    ),
-                    SizedBox(height: 40.h),
-                    Align(
-                      alignment: AlignmentDirectional.centerStart,
-                      child: Text(
-                        LocaleKeys.login_email_label.tr(),
-                        style: AppTextStyles.black16w500TextStyle,
-                      ),
-                    ),
-                    CustomTextField(
-                      controller: _emailController,
-                      hintText: "email@university.edu",
-                      validator: AppValidators.validateEmail,
-                      textInputType: TextInputType.emailAddress,
-                    ),
-                    SizedBox(height: 16.h),
-                    Align(
-                      alignment: AlignmentDirectional.centerStart,
-                      child: Text(
-                        LocaleKeys.login_password_label.tr(),
-                        style: AppTextStyles.black16w500TextStyle,
-                      ),
-                    ),
-                    CustomTextField(
-                      controller: _passwordController,
-                      hintText: "••••••••",
-                      isPassword: true,
-                      validator: AppValidators.validatePassword,
-                    ),
-                    SizedBox(height: 32.h),
-                    PrimaryButtonWidget(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          // TODO: Implement Login Logic
-                        }
-                      },
-                      text: LocaleKeys.login_submit_button.tr(),
-                    ),
-                    SizedBox(height: 24.h),
-                    Text(
-                      LocaleKeys.login_onboarding_message.tr(),
-                      style: AppTextStyles.grey12w500TextStyle,
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 24.h),
-                  ],
+              child: BlocListener<AuthCubit, AuthState>(
+                listener: (context, state) {
+                  if (state is AuthAuthenticated) {
+                    GoRouter.of(
+                      context,
+                    ).pushReplacement(AppRoutes.employeeDashboardScreen);
+                  } else if (state is AuthNeedActivation) {
+                    GoRouter.of(context).pushNamed(AppRoutes.onboardingScreen);
+                  } else if (state is AuthError) {
+                    showAnimatedSnakDialogue(
+                      context,
+                      type: AnimatedSnackBarType.error,
+                      message: state.failure.message,
+                    );
+                  }
+                },
+                child: Form(
+                  key: _formKey,
+                  child: BlocBuilder<AuthCubit, AuthState>(
+                    builder: (context, state) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(height: 48.h),
+                          Image.asset(
+                            AppImages.universityLogo,
+                            height: 120.sp,
+                            width: 120.sp,
+                          ),
+                          SizedBox(height: 35.h),
+                          Text(
+                            LocaleKeys.login_title.tr(),
+                            style: AppTextStyles.black24w600TextStyle,
+                          ),
+                          SizedBox(height: 10.h),
+                          Text(
+                            LocaleKeys.login_subtitle.tr(),
+                            style: AppTextStyles.grey18w400TextStyle,
+                          ),
+                          SizedBox(height: 40.h),
+                          Align(
+                            alignment: AlignmentDirectional.centerStart,
+                            child: Text(
+                              LocaleKeys.login_email_label.tr(),
+                              style: AppTextStyles.black16w500TextStyle,
+                            ),
+                          ),
+                          CustomTextField(
+                            controller: _emailController,
+                            hintText: "email@university.edu",
+                            validator: AppValidators.validateEmail,
+                            textInputType: TextInputType.emailAddress,
+                            isEnabled: state is! AuthLoading,
+                          ),
+                          SizedBox(height: 16.h),
+                          Align(
+                            alignment: AlignmentDirectional.centerStart,
+                            child: Text(
+                              LocaleKeys.login_password_label.tr(),
+                              style: AppTextStyles.black16w500TextStyle,
+                            ),
+                          ),
+                          CustomTextField(
+                            controller: _passwordController,
+                            hintText: "Enter at least 8 characters",
+                            isPassword: true,
+                            validator: AppValidators.validatePassword,
+                            isEnabled: state is! AuthLoading,
+                          ),
+                          SizedBox(height: 32.h),
+                          PrimaryButtonWidget(
+                            isLoading: state is AuthLoading,
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                context.read<AuthCubit>().login(
+                                  LoginBodyModel(
+                                    email: _emailController.text.trim(),
+                                    password: _passwordController.text,
+                                  ),
+                                );
+                              }
+                            },
+                            text: LocaleKeys.login_submit_button.tr(),
+                          ),
+
+                          SizedBox(height: 24.h),
+                          Text(
+                            LocaleKeys.login_onboarding_message.tr(),
+                            style: AppTextStyles.grey12w500TextStyle,
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 24.h),
+                        ],
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
