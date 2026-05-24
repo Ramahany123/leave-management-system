9524dc02ba6dcddf5b0570b7a1b648786f7ac938
Rama Hany <hanyrama703@gmai.com>
2026-05-24 05:44:10 +0300

    feat: refactor logout dialog into a separate widget and add personal info bottom sheet for enhanced user experience

--- lib/core/utils/app_dialogs.dart
+++ lib/core/utils/app_dialogs.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:leave_management_system/core/styles/app_colors.dart';
-
-import '../styles/app_text_styles.dart';
-import '../widgets/outlined_button_widget.dart' show OutlinedButtonWidget;
-import '../widgets/primary_button_widget.dart';
+import 'package:leave_management_system/core/models/user_model.dart';
+import 'package:leave_management_system/core/widgets/logout_dialog.dart';
+import 'package:leave_management_system/features/profile/ui/widgets/personal_info_bottom_sheet.dart';
 
 class AppDialogs {
   static void showLogoutDialog(
@@ -13,65 +10,16 @@ class AppDialogs {
   }) {
     showDialog(
       context: context,
-      builder: (context) => AlertDialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadiusGeometry.circular(16.r),
-        ),
-        backgroundColor: AppColors.whiteColor,
-        contentPadding: EdgeInsets.all(24.r),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: AppColors.errorRed.withValues(alpha: 0.1),
-              ),
-              child: Icon(Icons.logout, color: AppColors.errorRed, size: 32.sp),
-            ),
-            SizedBox(height: 16.h),
-            // Text
-            Text("Logout", style: AppTextStyles.black20w600TextStyle),
-            SizedBox(height: 8.h),
-            Text(
-              "Are you sure you want to log out of your account?",
-              textAlign: TextAlign.center,
-              style: AppTextStyles.grey14w400TextStyle,
-            ),
-            SizedBox(height: 24.h),
-            // Actions
-            Row(
-              children: [
-                Expanded(
-                  child: OutlinedButtonWidget(
-                    height: 48.h,
-                    onPressed: () => Navigator.pop(context),
-                    child: Text(
-                      "Cancel",
-                      style: AppTextStyles.primary14w600TextStyle,
-                    ),
-                  ),
-                ),
-                SizedBox(width: 12.w),
-                Expanded(
-                  child: PrimaryButtonWidget(
-                    height: 48.h,
-                    backgroundColor: AppColors.errorRed,
-                    onPressed: () {
-                      Navigator.pop(context);
-                      onConfirm();
-                    },
-                    child: Text(
-                      "Logout",
-                      style: AppTextStyles.white14w500TextStyle,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      builder: (context) => LogoutDialog(onConfirm: onConfirm),
+    );
+  }
+
+  static void showPersonalInfoSheet(BuildContext context, UserModel user) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
+      builder: (context) => PersonalInfoBottomSheet(user: user),
     );
   }
 }
---
+++ lib/features/profile/ui/widgets/personal_info_bottom_sheet.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:leave_management_system/core/models/user_model.dart';
+
+import '../../../../core/styles/app_colors.dart';
+import '../../../../core/styles/app_text_styles.dart';
+import '../../../../core/utils/date_extension.dart';
+import '../../../../core/widgets/key_value_row_widget.dart';
+
+class PersonalInfoBottomSheet extends StatelessWidget {
+  final UserModel user;
+  const PersonalInfoBottomSheet({super.key, required this.user});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.whiteColor,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(24.r)),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 16.h),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            height: 4.h,
+            width: 40.w,
+            decoration: BoxDecoration(
+              color: AppColors.cardBorderColor,
+              borderRadius: BorderRadius.circular(2.r),
+            ),
+          ),
+          SizedBox(height: 24.h),
+          Row(
+            //TODO: localize texts
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Personal Information",
+                style: AppTextStyles.black20w600TextStyle,
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(Icons.close, color: AppColors.greyColor),
+              ),
+            ],
+          ),
+          SizedBox(height: 24.h),
+          _buildInfoSection("OFFICIAL DETAILS", [
+            KeyValueRow(label: "National ID (SSN)", value: user.ssn),
+            KeyValueRow(
+              label: "Birth Date",
+              value: user.dateOfBirth.toReadableDate,
+            ),
+            KeyValueRow(label: "Gender", value: user.gender),
+          ]),
+          SizedBox(height: 24.h),
+
+          _buildInfoSection("UNIVERSITY DETAILS", [
+            KeyValueRow(
+              label: "Hire Date",
+              value: user.hireDate.toReadableDate,
+            ),
+            KeyValueRow(
+              label: "Years of Service",
+              value: "${user.yearsOfService} Years",
+            ),
+            KeyValueRow(label: "Role", value: user.role),
+            KeyValueRow(label: "Category", value: user.userType),
+            KeyValueRow(label: "Work Place", value: user.workplace),
+            KeyValueRow(label: "Job Title", value: user.jobTitle),
+          ]),
+
+          SizedBox(height: 32.h), // Bottom padding for safety
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInfoSection(String title, List<Widget> children) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: AppTextStyles.grey12w500TextStyle.copyWith(letterSpacing: 1.1),
+        ),
+        SizedBox(height: 12.h),
+        ...children,
+      ],
+    );
+  }
+}
