6e63cb6f8c8c188e975d6f895efa2e087bbf051d
Rama Hany <hanyrama703@gmai.com>
2026-05-29 12:28:34 +0300

    feat: refactor ProfileRepo to extend ChangeNotifier and manage currentUser state

--- lib/features/profile/data/repo/profile_repo.dart
+++ lib/features/profile/data/repo/profile_repo.dart
@@ -1,10 +1,17 @@
+import 'package:flutter/widgets.dart';
+import 'package:leave_management_system/core/models/user_model.dart';
 import 'package:leave_management_system/core/networking/api_error_handler.dart';
 import 'package:leave_management_system/core/utils/result.dart';
 import 'package:leave_management_system/features/profile/data/models/profile_response_model.dart';
+import 'package:leave_management_system/features/profile/data/models/update_contact_body_model.dart';
+import 'package:leave_management_system/features/profile/data/models/update_contact_response_model.dart';
 import 'package:leave_management_system/features/profile/data/web_services/profile_web_services.dart';
 
-class ProfileRepo {
+class ProfileRepo extends ChangeNotifier {
   final ProfileWebServices _profileWebServices;
+
+  UserModel? _currentUser;
+  UserModel? get currentUser => _currentUser;
 
   ProfileRepo({required ProfileWebServices profileWebServices})
     : _profileWebServices = profileWebServices;
@@ -12,6 +19,28 @@ class ProfileRepo {
   Future<Result<ProfileResponseModel>> getProfile() async {
     try {
       final response = await _profileWebServices.getProfile();
+      _currentUser = response.user;
+      notifyListeners();
+      return SuccessResult(response);
+    } catch (e) {
+      return FailureResult(ApiErrorHandler.handle(e));
+    }
+  }
+
+  Future<Result<UpdateContactResponseModel>> updateContact(
+    UpdateContactBodyModel updateContactBody,
+  ) async {
+    try {
+      final response = await _profileWebServices.updateContact(
+        updateContactBody,
+      );
+      if (_currentUser != null) {
+        _currentUser = _currentUser!.copyWith(phone: response.phone);
+      } else {
+        await getProfile();
+      }
+      notifyListeners();
+
       return SuccessResult(response);
     } catch (e) {
       return FailureResult(ApiErrorHandler.handle(e));
