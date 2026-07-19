64e8949b45cb3499957618ed0adf573fb0ea4a5f
Rama Hany <hanyrama703@gmai.com>
2026-07-20 02:03:42 +0300

    feat: add manager coverage and reports screens

---
+++ lib/features/manager_coverage/ui/screens/manager_coverage_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ManagerCoverageScreen extends StatelessWidget {
+  const ManagerCoverageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: Text("manager coverage screen")));
+  }
+}
---
+++ lib/features/manager_reports/ui/screens/manager_reports_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ManageReportsScreen extends StatelessWidget {
+  const ManageReportsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: Text("manager reports screen")));
+  }
+}
