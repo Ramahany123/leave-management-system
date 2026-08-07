010ca4dddec91dad113d1682710e226acdd7988d
Rama Hany <hanyrama703@gmai.com>
2026-08-07 03:57:18 +0300

    feat: enhance date picker helper to accept custom first and last date parameters

--- lib/core/utils/date_picker_helper.dart
+++ lib/core/utils/date_picker_helper.dart
@@ -5,17 +5,16 @@ class DatePickerHelper {
   static Future<DateTime?> pickDate(
     BuildContext context, {
     DateTime? initialDate,
+    DateTime? firstDate,
+    DateTime? lastDate,
   }) async {
     return await showDatePicker(
       context: context,
       initialDate: initialDate ?? DateTime.now(),
-      firstDate: DateTime.now().subtract(const Duration(days: 30)),
-      lastDate: DateTime.now().add(const Duration(days: 365)),
+      firstDate: firstDate ?? DateTime.now().subtract(const Duration(days: 30)),
+      lastDate: lastDate ?? DateTime.now().add(const Duration(days: 365)),
       builder: (datePickerContext, child) {
-        return Theme(
-          data: context.theme,
-          child: child!,
-        );
+        return Theme(data: context.theme, child: child!);
       },
     );
   }
